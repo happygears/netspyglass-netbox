d98a3cac2d72bc4a53bde96f27e2aceb7b9f4d2e
Siva Prasad Naidu <naidu@happygears.net>
2022-02-22 21:31:48 +0530

    code clean-up

--- nsg-cmdb.py
+++ nsg-cmdb.py
@@ -35,37 +35,29 @@ class NsgCmdbIntegration:
                 logging.StreamHandler()
             ])
         self.log = logging.getLogger('nsg-cmdb')
-        print("========= get_nsg_device")
-        print("========= url : ", self.config["nsg_settings"]["nsg_url"])
-        print("========= nsg_token: ", self.config["nsg_settings"]["nsg_token"])
-        print("========= netid: ", self.config["nsg_settings"]["netid"])
-        self.nsg = nsgapi.NsgAPI(self.log,
-                            self.config["nsg_settings"]["nsg_url"],
-                            self.config["nsg_settings"]["nsg_token"],
-                            self.config["nsg_settings"]["netid"],
-                           )
+        self.log_nsgapi = logging.getLogger('nsgapi')
+        # self.log_servicenow = logging.getLogger('nsg-servicenow')
+        self.log.info("NSG URL : {}".format(self.config["nsg_settings"]["nsg_url"]))
+        self.log.info("NSG Token : {}".format(self.config["nsg_settings"]["nsg_token"]))
+        self.log.info("NSG Network ID : {}".format(self.config["nsg_settings"]["netid"]))
+        self.nsg = nsgapi.NsgAPI(self.log_nsgapi,
+                                 self.config["nsg_settings"]["nsg_url"],
+                                 self.config["nsg_settings"]["nsg_token"],
+                                 self.config["nsg_settings"]["netid"],
+                                )
 
     def log_args(self):
         self.log.info(self.args)
 
     def start(self):
-        self.log.info('CMDB-NetSpyGlass integration script starts')
+        self.log.info('CMDB-NetSpyGlass integration starts here.......')
         get_cmdb_type = self.config["cmdb_settings"]["cmdb_type"]
         self.log.info("CMDB Type : {}".format(get_cmdb_type))
-
-        '''
-        if get_cmdb_type == 'servicenow':
-            self.run(module_name='nsg-servicenow', class_name='ServiceNow')
-        elif get_cmdb_type == 'freshworks':
-            self.run(module_name='nsg-freshworks', class_name='FreshWorks')
-        else:
-            self.log.info("CMDB Type : {} Not Supported".format(get_cmdb_type))
-        '''
         
         module_name = VENDOR_SUPPORT.get(get_cmdb_type)[0]
         class_name = VENDOR_SUPPORT.get(get_cmdb_type)[1]
-        print("module_name : ", module_name)
-        print("class_name : ", class_name)
+        self.log.info("vendor module name : {}".format(module_name))
+        self.log.info("vendor class name : {}".format(class_name))
         self.run(module_name, class_name)
 
         # # maintain the process indefinitely
@@ -76,53 +68,50 @@ class NsgCmdbIntegration:
         #     return
     
     def run(self, module_name, class_name):
-        #__import__ method used
-        # to fetch module
+        #__import__ method used to fetch module
         module = __import__(module_name)
 
-        # getting attribute by
-        # getattr() method
+        # getting attribute by getattr() method
         my_class = getattr(module, class_name)
-        # my_class.welcome('User_1')
         
         cmdb_devices_operational, cmdb_devices_retired = my_class.runs(self)
-        # cmdb_devices = my_class.get_snow_cmdb_devices(self)
-        self.log.info("output in main file")
-        self.log.info("==============OPERATIONAL================")
-        print(cmdb_devices_operational)
-        self.log.info("==============RETIRED================")
-        print(cmdb_devices_retired)
-        self.log.info("==============================")
 
-        # self.log = logging.getLogger('nsg-servicenow')
-        # nsg_devices = my_class.get_nsg_device(self)
-        # self.log = logging.getLogger('nsg-cmdb')
+        self.log.info("============== START >> OPERATIONAL DEVICES << START =============")
+        self.log.info('{}'.format(cmdb_devices_operational))
+        self.log.info("============== END >> OPERATIONAL DEVICES << END =================")
+
+        self.log.info("============== START >> RETIRED DEVICES << START =================")
+        self.log.info('{}'.format(cmdb_devices_retired))
+        self.log.info("============== END >> RETIRED DEVICES << END =====================")
+
         nsg_devices = self.get_nsg_device()
-        self.log.info('NetSpyGlass: {0} devices'.format(len(nsg_devices)))
 
         cmdb_dev_set = set(cmdb_devices_operational.keys())
         self.log.info('cmdb_dev_set: {0}'.format(cmdb_dev_set))
+
         nsg_dev_set = set(nsg_devices.keys())
         self.log.info('nsg_dev_set: {0}'.format(nsg_dev_set))
 
         to_add = set.difference(cmdb_dev_set, nsg_dev_set)  # set of addresses as strings
-        self.log.info('TO_ADD:    {0}'.format(to_add))
+        self.log.info('TO_ADD: {0}'.format(to_add))
 
         to_remove = set.difference(nsg_dev_set, cmdb_dev_set)  # set of addresses as strings
-        self.log.info('TO_REMOVE-1: {0}'.format(to_remove))
+        self.log.info('TO_REMOVE-ALL_DEVICES: {0}'.format(to_remove))
         
-        # additional check // Naidu
+        # additional check || This gets the CMDB "retired" devices
         cmdb_retired_dev_set = set(cmdb_devices_retired.keys())
-        self.log.info('cmdb_retired_dev_set: {0}'.format(cmdb_retired_dev_set))
+        self.log.info('CMDB Retired devices: {0}'.format(cmdb_retired_dev_set))
 
-
+        # This block checks for flag "skip_delete_non_cmdb_devices"
+        # If "skip_delete_non_cmdb_devices" = True, skips delete devices that are added in NSG directly
+        # i.e. deletes only devices that are marked as "retired" in SNOW CMDB
         if self.config["nsg_settings"]["skip_delete_non_cmdb_devices"]:
-            # This deletes devices that are marked as "retired" in SNOW CMDB
             to_remove = set.intersection(to_remove, cmdb_retired_dev_set)  # set of addresses as strings
-            self.log.info('TO_REMOVE-2: {0}'.format(to_remove))
+            self.log.info('TO_REMOVE-NON_CMDB_DEVICES: {0}'.format(to_remove))
 
 
-        self.log.info('TO_ADD_FINAL:    {0}'.format(to_add))
+        # This logs final set of devices that are added/deleted in NSG
+        self.log.info('TO_ADD_FINAL   : {0}'.format(to_add))
         self.log.info('TO_REMOVE_FINAL: {0}'.format(to_remove))
 
         if to_add:
@@ -142,7 +131,6 @@ class NsgCmdbIntegration:
             return
         
         nsg_devices = self.nsg.get_devices()
-        # self.log = logging.getLogger('nsg-servicenow')
         self.log.info('NetSpyGlass: {0} devices'.format(len(nsg_devices)))
         self.log.info('nsg_devices : {}'.format(nsg_devices))
         return nsg_devices
@@ -160,12 +148,6 @@ class NsgCmdbIntegration:
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    # parser.add_argument('--netbox-url', required=True)
-    # parser.add_argument('--netbox-token', required=False)
-    # parser.add_argument('--nsg-url', required=True)
-    # parser.add_argument('--nsg-token', required=True)
-    # parser.add_argument('--channel', required=True,
-    #                     help='NetSpyGlass communication channel name to use with all imported devices')
     parser.add_argument('--config', required=True, help='Config yaml file that lists the configurations')
     parser.add_argument('--netid', required=False,
                         default=1,
--- nsg-servicenow.py
+++ nsg-servicenow.py
@@ -1,264 +1,67 @@
-'''
-class ServiceNow:
-    def welcome(str):
-        print("Hi ! % s Welcome to ServiceNow" % str)
-'''
 
-import argparse
-import ipaddress
 import logging
-import os
-# import pynetbox
-import sched
-import sys
-import time
-import urllib3
-import yaml
 import requests
 from requests.auth import HTTPBasicAuth
 
-import nsgapi
-
 class ServiceNow:
-
-    def __init__(self, log) -> None:
-        self.log = logging.getLogger('nsg-servicenow')
     
     def runs(self):
         try:
-            self.log.info(self.args)
-            print("Hello Naidu")
-            # my_class.get_snow_cmdb_devices(self)
-            # get_snow_cmdb_devices(self)
-            # snow_cmdb_devices = self.get_snow_cmdb_devices()
-            # print("snow_cmdb_devices : ", snow_cmdb_devices)
+            self.log_snow = logging.getLogger('nsg-servicenow')
+            self.log_snow.info(self.args)
+            self.log_snow.info("nsg-servicenow API processing starts here.....")
 
-            def get_snow_cmdb_devices(self):
-                print("get_snow_cmdb_devices")
-                # Set the request parameters
-                url = self.config["cmdb_settings"]["cmdb_url"]
-                user = self.config["cmdb_settings"]["cmdb_username"]
-                pwd = self.config["cmdb_settings"]["cmdb_password"]
+            # Set the request parameters
+            url = self.config["cmdb_settings"]["cmdb_url"]
+            user = self.config["cmdb_settings"]["cmdb_username"]
+            pwd = self.config["cmdb_settings"]["cmdb_password"]
+            self.log_snow.info("CMDB URL : {}".format(self.config["cmdb_settings"]["cmdb_url"]))
+            self.log_snow.info("CMDB Username : {}".format(self.config["cmdb_settings"]["cmdb_username"]))
+            self.log_snow.info("CMDB Password : {}".format(self.config["cmdb_settings"]["cmdb_password"]))
 
+            # Set proper headers
+            headers = {"Accept": "application/json", "Content-Type": "application/json"}
+
+            def get_snow_cmdb_devices(url, device_status):
+                operational_status = { "operational" : 1, "retired" : 6}
+                device_status = operational_status.get(device_status)
+
+                query_url = '?asset_tag=nsg-cmdb-testing&operational_status={}'.format(device_status)
+                url = ''.join([url, query_url])
+
+                # Do the HTTP request
+                response = requests.get(url, auth=(user, pwd), headers=headers)
+                print("Response code : ", response.status_code)
+
+                # Check for HTTP codes other than 200
+                if response.status_code != 200: 
+                    print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:', response.content)
+                    exit()
                 
+                response = response.json()
+                # print("Response Type :", type(response))
 
-                def get_devices(url, device_status):
-                    operational_status = { "operational" : 1, "retired" : 6}
-                    device_status = operational_status.get(device_status)
-
-                    query_url = '?asset_tag=nsg-cmdb-testing&operational_status={}'.format(device_status)
-                    url = ''.join([url, query_url])
-
-                    # url = 'https://dev78298.service-now.com/api/now/table/cmdb_ci?asset_tag=nsg-cmdb-testing&operational_status=6'
-                    # print("url : ", url)
-                    
-                    # Set proper headers
-                    headers = {"Accept": "application/json", "Content-Type": "application/json"}
-                    
-                    # Do the HTTP request
-                    response = requests.get(url, auth=(user, pwd), headers=headers)
-                    print("Response code : ", response.status_code)
-
-                    # Check for HTTP codes other than 200
-                    if response.status_code != 200: 
-                        print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:', response.content)
-                        exit()
-                    
-                    response = response.json()
-                    print("Response Type :", type(response))
-
-                    '''
-                    {
-                        "result": [
-                            {
-                            "attested_date": "",
-                            "skip_sync": "false",
-                            "operational_status": "6",
-                            "sys_updated_on": "2022-02-11 10:26:46",
-                            "attestation_score": "",
-                            "discovery_source": "",
-                            "first_discovered": "",
-                            "sys_updated_by": "admin",
-                            "due_in": "",
-                            "sys_created_on": "2022-02-11 05:35:06",
-                            "sys_domain": {
-                                "link": "https://dev78298.service-now.com/api/now/table/sys_user_group/global",
-                                "value": "global"
-                            },
-                            "install_date": "",
-                            "gl_account": "",
-                            "invoice_number": "",
-                            "sys_created_by": "admin",
-                            "warranty_expiration": "",
-                            "asset_tag": "nsg-cmdb-testing",
-                            "fqdn": "",
-                            "change_control": "",
-                            "owned_by": "",
-                            "checked_out": "",
-                            "sys_domain_path": "/",
-                            "business_unit": "",
-                            "delivery_date": "",
-                            "maintenance_schedule": "",
-                            "install_status": "1",
-                            "cost_center": "",
-                            "attested_by": "",
-                            "supported_by": "",
-                            "dns_domain": "",
-                            "name": "iad1-bb01",
-                            "assigned": "",
-                            "life_cycle_stage": "",
-                            "purchase_date": "",
-                            "subcategory": "IP",
-                            "short_description": "",
-                            "assignment_group": "",
-                            "managed_by": "",
-                            "managed_by_group": "",
-                            "can_print": "false",
-                            "last_discovered": "",
-                            "sys_class_name": "cmdb_ci_ip_router",
-                            "manufacturer": "",
-                            "sys_id": "73d994e20721011074e4ff4c7c1ed065",
-                            "po_number": "",
-                            "checked_in": "",
-                            "sys_class_path": "/!!/!2/!!/!.",
-                            "life_cycle_stage_status": "",
-                            "mac_address": "",
-                            "vendor": "",
-                            "company": "",
-                            "justification": "",
-                            "model_number": "",
-                            "department": "",
-                            "assigned_to": "",
-                            "start_date": "",
-                            "comments": "",
-                            "cost": "",
-                            "sys_mod_count": "3",
-                            "monitor": "false",
-                            "serial_number": "",
-                            "ip_address": "10.10.10.3",
-                            "model_id": {
-                                "link": "https://dev78298.service-now.com/api/now/table/cmdb_model/d92cdce60721011074e4ff4c7c1ed035",
-                                "value": "d92cdce60721011074e4ff4c7c1ed035"
-                            },
-                            "duplicate_of": "",
-                            "sys_tags": "",
-                            "cost_cc": "USD",
-                            "order_date": "",
-                            "schedule": "",
-                            "support_group": "",
-                            "environment": "",
-                            "due": "",
-                            "attested": "false",
-                            "correlation_id": "",
-                            "unverified": "false",
-                            "attributes": "",
-                            "location": "",
-                            "asset": {
-                                "link": "https://dev78298.service-now.com/api/now/table/alm_asset/552cdce60721011074e4ff4c7c1ed037",
-                                "value": "552cdce60721011074e4ff4c7c1ed037"
-                            },
-                            "category": "Resource",
-                            "fault_count": "0",
-                            "lease_id": ""
-                            }
-                        ]
-                    }
-                    '''
-                    devices = response.get("result")
-                    final_dict = {}
-                    for device in devices:
-                        sub_dict = {}
-                        # print("Type =====> : ", type(device))
-                        # print(device)
-                        sub_dict['name'] = device['name']
-                        sub_dict['ip_address'] = device['ip_address']
-                        sub_dict['asset_tag'] = device['asset_tag']
-                        sub_dict['sys_class_name'] = device['sys_class_name']
-                        sub_dict['operational_status'] = device['operational_status']
-                        final_dict[device['ip_address']] = sub_dict
-                    return final_dict
+                devices = response.get("result")
+                final_dict = {}
+                for device in devices:
+                    sub_dict = {}
+                    sub_dict['name'] = device['name']
+                    sub_dict['ip_address'] = device['ip_address']
+                    sub_dict['asset_tag'] = device['asset_tag']
+                    sub_dict['sys_class_name'] = device['sys_class_name']
+                    sub_dict['operational_status'] = device['operational_status']
+                    final_dict[device['ip_address']] = sub_dict
+                return final_dict
 
 
-                snow_cmdb_devices_operational = get_devices(url, device_status='operational')
-                print("get_snow_cmdb_devices_operational :", snow_cmdb_devices_operational)
-                
-                snow_cmdb_devices_retired = get_devices(url, device_status='retired')
-                print("get_snow_cmdb_devices_retired :", snow_cmdb_devices_retired)
-
-                return snow_cmdb_devices_operational, snow_cmdb_devices_retired
+            devices_operational = get_snow_cmdb_devices(url, device_status='operational')
+            self.log_snow.info("Operational Devices : {}".format(devices_operational))
             
-            snow_cmdb_devices_operational, snow_cmdb_devices_retired = get_snow_cmdb_devices(self)
-            
-            return snow_cmdb_devices_operational, snow_cmdb_devices_retired 
+            devices_retired = get_snow_cmdb_devices(url, device_status='retired')
+            self.log_snow.info("Retired Devices : {}".format(devices_retired))
 
+            return devices_operational, devices_retired
 
         except Exception as e:
-            self.log.exception('Unknown exception: %s', e)
-
-    # def get_nsg_device(self):
-        
-    #     print("========= get_nsg_device")
-    #     print("========= url : ", self.config["nsg_settings"]["nsg_url"])
-    #     print("========= nsg_token: ", self.config["nsg_settings"]["nsg_token"])
-    #     print("========= netid: ", self.config["nsg_settings"]["netid"])
-    #     nsg = nsgapi.NsgAPI(self.log,
-    #                         self.config["nsg_settings"]["nsg_url"],
-    #                         self.config["nsg_settings"]["nsg_token"],
-    #                         self.config["nsg_settings"]["netid"],
-    #                        )
-    #     # tasks = nsg.get_tasks()
-    #     # if tasks:
-    #     #     self.log.info('NetSpyGlass: tasks: {0}'.format(len(tasks)))
-    #     #     self.log.warning('skipping cycle because of an active NSG background task')
-    #     #     return
-        
-    #     nsg_devices = nsg.get_devices()
-    #     # self.log = logging.getLogger('nsg-servicenow')
-    #     self.log.info('NetSpyGlass: {0} devices'.format(len(nsg_devices)))
-    #     self.log.info('nsg_devices : {}'.format(nsg_devices))
-    #     return nsg_devices
-    
-    # def get_snow_cmdb_devices(self):
-    #     print("get_snow_cmdb_devices")
-    #     # Set the request parameters
-    #     url = self.config["cmdb_settings"]["cmdb_url"]
-    #     user = self.config["cmdb_settings"]["cmdb_username"]
-    #     pwd = self.config["cmdb_settings"]["cmdb_password"]
-
-        
-
-    #     def get_devices(url, device_status):
-    #         operational_status = { "operational" : 1, "retired" : 6}
-    #         device_status = operational_status.get(device_status)
-
-    #         query_url = '?asset_tag=nsg-cmdb-testing&operational_status={}'.format(device_status)
-    #         url = ''.join([url, query_url])
-
-    #         # url = 'https://dev78298.service-now.com/api/now/table/cmdb_ci?asset_tag=nsg-cmdb-testing&operational_status=6'
-    #         # print("url : ", url)
-            
-    #         # Set proper headers
-    #         headers = {"Accept": "application/json", "Content-Type": "application/json"}
-            
-    #         # Do the HTTP request
-    #         response = requests.get(url, auth=(user, pwd), headers=headers)
-    #         print("Response code : ", response.status_code)
-
-    #         # Check for HTTP codes other than 200
-    #         if response.status_code != 200: 
-    #             print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:', response.content)
-    #             exit()
-            
-    #         response = response.json()
-    #         print("Response Type :", type(response))
-    #         return response
-
-
-    #     # snow_cmdb_devices = get_devices(url, device_status='operational')
-    #     # print("get_operational_devices :", snow_cmdb_devices)
-        
-    #     snow_cmdb_devices = get_devices(url, device_status='retired')
-    #     print("get_retired_devices :", snow_cmdb_devices)
-
-    #     return snow_cmdb_devices
+            self.log_snow.exception('Unknown exception: %s', e)
 
